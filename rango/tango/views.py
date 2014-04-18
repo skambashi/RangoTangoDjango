d2dc3aeddddec33fcd8edbe38a8dcbe8ecafe61a
Seikun Kambashi <seikun_lee@hotmail.com>
2014-04-18 17:42:05 -0400

    trying multiple url mappings with really basic views - no templates yet

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Tango says: HOLA, TODO EL MUNDO! <a href = '/tango/about'>About</a>")
+
+def about(request):
+	return HttpResponse("Tango says: WELCOME TO THE ABOUTS. <a href='/tango/'>Index</a>")
